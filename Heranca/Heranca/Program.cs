384fcba1bb3c5f5466f862d67a0910fa97fa3233
Caíque Gomes <111620241+caiquegomesdev@users.noreply.github.com>
2024-02-07 21:41:37 -0300

    Estudos  em C#

---
+++ Heranca/Heranca/Program.cs
@@ -0,0 +1,16 @@
+﻿using Heranca.Entities;
+using System;
+namespace heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob James Marley Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200.0; // error
+        }
+    }
+}
