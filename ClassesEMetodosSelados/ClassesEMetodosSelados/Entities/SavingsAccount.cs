7fb14bc7f911142ca9e450be7bbf383118e4296f
Caíque Gomes <111620241+caiquegomesdev@users.noreply.github.com>
2024-02-09 23:04:30 -0300

    Estudos em C#

---
+++ ClassesEMetodosSelados/ClassesEMetodosSelados/Entities/SavingsAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesEMetodosSelados.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public sealed override void Withdraw(double amount) //sealed deixa a linha de codigo selada serve para garantir seguranca e ganho de performace
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
