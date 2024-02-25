e22fd612035629c99ad81ff42498f15f9586906c
Caíque Gomes <111620241+caiquegomesdev@users.noreply.github.com>
2024-02-25 12:28:58 -0300

    Estudos em C#

---
+++ FileFileInfoIOExeception/FileFileInfoIOExeception/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace FileFileInfoIOException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Windows\Temp\file1.txt";
+            string targetPath = @"C:\Windows\Temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
