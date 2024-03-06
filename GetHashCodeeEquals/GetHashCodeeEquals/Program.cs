cd4a3580f7b4e2dbbbdbedc9c459b11b4b1f186c
Caíque Gomes <111620241+caiquegomesdev@users.noreply.github.com>
2024-03-05 23:50:21 -0300

    Estudos em C#

---
+++ GetHashCodeeEquals/GetHashCodeeEquals/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GetHashCodeeEquals.Entities;
+
+namespace GetHashCodeeEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = " Maria ", Email = "maria@gmail.com" };
+            Client b = new Client { Name = " Gomes ", Email = "gomes@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
