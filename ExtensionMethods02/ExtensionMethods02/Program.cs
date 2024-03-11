fa77918ed5fe8d8f206b6484f833ab213c2b3088
Caique Gomes <caiquegomesdev@gmail.com>
2024-03-10 23:06:28 -0300

    Estudos em C#

---
+++ ExtensionMethods02/ExtensionMethods02/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class ProcessFile
+{
+    public static void Main()
+    {
+        string s1 = "Bom dia queridos Estudantes";
+        Console.WriteLine(s1.Cut(10));
+    }
+}
