eb4f5e59f3cca447e7787f02b68272d334569cb5
Caique Gomes <caiquegomesdev@gmail.com>
2024-03-10 23:00:07 -0300

    Estudos em C#

---
+++ ExtensionMethods01/ExtensionMethods01/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class ProcessFile
+{
+    public static void Main()
+    {
+        DateTime dt = new DateTime(2024, 03, 10, 22, 8, 45);
+        Console.WriteLine(dt.ElapsedTime());
+    }
+}
