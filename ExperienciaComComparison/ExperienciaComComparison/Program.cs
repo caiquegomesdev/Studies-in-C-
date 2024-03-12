edc486450ac1db6956f9b4f865eadbc81db17d8b
Caique Gomes <caiquegomesdev@gmail.com>
2024-03-11 22:51:04 -0300

    Estudos em C#

--- ExperienciaComComparison/ExperienciaComComparison/Program.cs
+++ ExperienciaComComparison/ExperienciaComComparison/Program.cs
@@ -15,12 +15,17 @@ namespace ExperienciaComComparison
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+            list.Sort(CompareProducts);
 
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
+        }
+
+        static int CompareProducts(Product p1, Product p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
     }
 }
