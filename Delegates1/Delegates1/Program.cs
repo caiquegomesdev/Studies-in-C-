ca84194231cdc842a600c4f9991e7a20732f6de3
Caique Gomes <caiquegomesdev@gmail.com>
2024-03-13 00:03:24 -0300

    Estudos em C#

--- Delegates1/Delegates1/Program.cs
+++ Delegates1/Delegates1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Delegates1.Services;
+using System;
+
+namespace Delegates1
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            // BinaryNumericOperationop= CalculationService.Sum;
+            BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+            // doubleresult= op(a, b);
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
