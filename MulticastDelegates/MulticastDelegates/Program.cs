d96ab40fc6b1b0e1194d0c60706a9105e8dd4693
Caique Gomes <caiquegomesdev@gmail.com>
2024-03-13 23:42:16 -0300

    Estudos em C#

---
+++ MulticastDelegates/MulticastDelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿using MulticastDelegates.Services;
+using System;
+
+namespace MulticastDelegates
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            op(a, b);
+        }
+    }
+}
